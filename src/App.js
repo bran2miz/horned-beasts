fb9f86be23da0344529edc6f26311712f9a7724b
brandonmizutani <bran2miz@gmail.com>
2021-09-20 19:10:44 -0700

    add hornedbeats.js which is another component for the main.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Header from './Components/Header.js';
 import Main from './Components/Main.js';
 import Footer from './Components/Footer.js';
---
+++ src/Components/HornedBeasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return (
+      <>
+        <h2>This horned beast is known as {this.props.title}</h2>
+        <img src={this.props.imgURL} alt='hornedBeast_img' title={this.props.title} />
+        <p>{this.props.description}</p>
+      </>
+    );
+  }
+}
+
+export default HornedBeast;
